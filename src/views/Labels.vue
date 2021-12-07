a6e24103c9124f37040b4f4bbe60dba159e8fe06
heycn <x1chennan@qq.com>
2021-12-07 23:56:00 +0800

    init

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" >
+  export default {
+    name: 'Labels'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" >
+  export default {
+    name: 'Statistics'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
