1a99d4e756f8fb94d94ea65868736af40801c1b4
heycn <x1chennan@qq.com>
2021-12-08 19:48:12 +0800

    优化icon】

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -18,14 +18,6 @@
 </template>
 
 <script lang="ts">
-// eslint-disable-next-line no-undef
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-try {
-  importAll(require.context('../assets/icons', true, /\.svg$/))
-} catch (error) {
-  console.log(error)
-}
-
 export default {
   name: 'Nav'
 }
