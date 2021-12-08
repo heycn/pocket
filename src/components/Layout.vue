056d48de7a365041316ff3f9c3165f5b6fc35c62
heycn <x1chennan@qq.com>
2021-12-08 20:21:11 +0800

    添加Nav样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -9,13 +9,11 @@
 
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .content {
-  border: solid 1px rgb(231, 24, 58);
   overflow: auto;
   flex-grow: 1;
 }
