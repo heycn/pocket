e66daaacf7acf22b9a7230f222e2916eb85318e1
heycn <x1chennan@qq.com>
2021-12-08 19:23:18 +0800

    -

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -14,9 +14,13 @@
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/tag.svg'
-
-console.log('打印:' + x)
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/))
+} catch (error) {
+  console.log(error)
+}
 
 export default {
   name: 'Nav'
