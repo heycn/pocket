cc60e12338940715b7ef7ba219700c23ecdc2ccd
heycn <x1chennan@qq.com>
2021-12-09 14:11:44 +0800

    添加notes

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,14 +15,14 @@
     <div>
       <label class="notes">
         <span class="name">备注</span>
-        <input type="text" />
+        <input type="text" placeholder="添加备注..." />
       </label>
     </div>
 
     <div>
       <ul class="types">
         <li class="selected">支出</li>
-        <li class="selected">收入</li>
+        <li>收入</li>
       </ul>
     </div>
 
@@ -72,14 +72,54 @@ export default {
       margin-right: 12px;
     }
   }
-  .new{
+  .new {
     padding-top: 16px;
-    button{
+    button {
       background: transparent;
       border: none;
       color: #999;
       border-bottom: 1px solid #666;
       padding: 0 4px;
+    }
+  }
+}
+.notes {
+  font-size: 14px;
+  background: #f6f6f6;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  li {
+    width: 50%;
+    line-height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: $color-highlight;
     }
   }
 }
