b6fcd1e32303ee3773bb80e684e5efb79e21b809
heycn <x1chennan@qq.com>
2021-12-09 21:20:59 +0800

    用TS写一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -8,40 +8,27 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-  type = '-' // '-'表示支出 '+'表示收入
+  type = '-'; // '-'表示支出 '+'表示收入
+
+  @Prop(Number) xxx: number | undefined;
+  // Prop 告诉 Vue, xxx 不是 data 是 prop
+  // Number 告诉 Vue, xxx 运行时是个 Number
+  // xxx 属性名
+  // number | undefined 就是 告诉 TS, xxx 的编译时类型
+
   selectType(type: string) {
     // type 只能是 '-' 和 '+' 中的一个
     if (type !== '-' && type !== '+') {
-      throw new Error('type is unknown')
+      throw new Error('type is unknown');
     }
-    this.type = type
+    this.type = type;
   }
 }
-// export default {
-//   name: 'Types',
-//   props: ['xxx'],
-//   data() {
-//     return {
-//       type: '-' // '-'表示支出 '+'表示收入
-//     }
-//   },
-//   mounted() {
-//     console.log(this.xxx)
-//   },
-//   methods: {
-//     selectType(type) { // type 只能是 '-' 和 '+' 中的一个
-//       if (type !== '-' && type !== '+') {
-//         throw new Error('type is unknown')
-//       }
-//       this.type = type
-//     }
-//   }
-// }
 </script>
 
 <style lang="scss" scoped>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,15 +8,15 @@
 </template>
 
 <script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue'
-import Types from '@/components/Money/Types.vue'
-import Notes from '@/components/Money/Notes.vue'
-import Tags from '@/components/Money/Tags.vue'
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Types from '@/components/Money/Types.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
 
 export default {
   name: 'Money',
   components: { NumberPad, Types, Notes, Tags }
-}
+};
 </script>
 
 <style lang="scss">
