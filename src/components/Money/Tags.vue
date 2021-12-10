b82a3930620d121f03bdb6a598319d0f2859cff2
heycn <x1chennan@qq.com>
2021-12-10 18:27:20 +0800

    可以选中或取消 tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,17 +4,34 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li
+        v-for="tag in dataSource"
+        :key="tag"
+        :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
+        @click="toggle(tag)"
+      >
+        {{ tag }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue {
+  @Prop() dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
 }
 </script>
 
@@ -29,7 +46,8 @@ export default {
     display: flex;
     flex-wrap: wrap;
     li {
-      background: #f6f6f6;
+      $bg: #f6f6f6;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -37,6 +55,10 @@ export default {
       padding: 0 16px;
       margin-right: 12px;
       margin: 4px;
+      &.selected {
+        background: darken($bg, 75%);
+        color: white;
+      }
     }
   }
   .new {
