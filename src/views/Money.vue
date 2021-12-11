ae6b97384846431b64ddbe802c122fabf5c954b6
heycn <x1chennan@qq.com>
2021-12-11 20:28:50 +0800

    无法保存数据到本地BUG已解决

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
-    <Types :value="record.type" @update:value="onUpdateType" />
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
-    <Tags :dataSource.sync="tags" @update:value="onUpdateTags" />
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
 
@@ -13,7 +13,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 
 const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
 
@@ -41,19 +41,20 @@ export default class Money extends Vue {
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
-  onUpdateType(value: string) {
-    this.record.type = value;
-  }
-  onUpdateAmount(value: string) {
-    this.record.amount = parseFloat(value);
-  }
+
   saveRecord() {
     const record2: Record = JSON.parse(JSON.stringify(this.record));
     record2.createdAt = new Date();
     this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
   }
 }
 </script>
