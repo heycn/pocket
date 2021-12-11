86438cfd744aba87603f432408f6ff9255dcc46c
heycn <x1chennan@qq.com>
2021-12-11 20:17:52 +0800

    提交BUG

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{ recordList }}
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     <Types :value="record.type" @update:value="onUpdateType" />
     <Notes @update:value="onUpdateNotes" />
