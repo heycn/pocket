68ab8bf15a0826f3d3ca65fa4dc0ca337f7efc6f
heycn <x1chennan@qq.com>
2021-12-14 01:50:59 +0800

    删除多余代码

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,7 @@
   <div>
     <label class="formItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="this.placeholder" />
+      <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="placeholder" />
     </label>
   </div>
 </template>
@@ -17,7 +17,6 @@ export default class FormItem extends Vue {
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;
 
-  @Watch('value')
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
