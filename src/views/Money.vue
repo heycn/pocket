54093749194192323658fa3cdcdfba1c1eda15c2
heycn <x1chennan@qq.com>
2021-12-14 19:36:35 +0800

    将所有数据操作封装在 store 里, 但是存在新建标签按钮没反应的 BUG

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,14 +16,15 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component({
   components: {NumberPad, Types, FormItem, Tags}
 })
 export default class Money extends Vue {
-  tags = window.tagList;
+  tags = store.tagList;
   // eslint-disable-next-line no-undef
-  recordList = window.recordList;
+  recordList = store.recordList;
   // eslint-disable-next-line no-undef
   record: RecordItem = {
     tags: [],
@@ -41,7 +42,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    window.createRecord(this.record);
+    store.createRecord(this.record);
   }
 }
 </script>
