2dbc991b386bc472403ee357ea38607e71135319
heycn <x1chennan@qq.com>
2021-12-14 21:00:16 +0800

    模块化的 store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,9 +1,11 @@
-import recordStore from './recordStore';
-import tagStore from './tagStore';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
   ...recordStore,
   ...tagStore
 };
+
+console.log(store);
 
 export default store;
--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,22 +1,21 @@
 import clone from '@/lib/clone';
 
 const localStorageKeyName = 'recordList';
-let data: RecordItem[] | undefined = undefined;
 
 const recordStore = {
-  recordList: data,
+  recordList: [] as RecordItem[],
   fetchRecords() {
-    data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-    return data;
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
   },
-  saveRecord() {
+  saveRecords() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
   },
   createRecord(record: RecordItem) {
     const record2: RecordItem = clone(record);
     record2.createdAt = new Date();
-    this.recordList?.push(record2);
-    recordStore.saveRecord();
+    this.recordList && this.recordList.push(record2);
+    recordStore.saveRecords();
   }
 };
 
