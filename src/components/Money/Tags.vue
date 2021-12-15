832d128ff858f461352531a645b52836566637be
heycn <x1chennan@qq.com>
2021-12-15 20:54:30 +0800

    Labels.vue 使用 Vuex，抽离 TagHelper 作为 mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id" :class="{selected: selectedTags.indexOf(tag) >= 0}" @click="toggle(tag)">
@@ -14,6 +14,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 @Component({
   computed: {
     tagList() {
@@ -21,7 +23,7 @@ import {Component} from 'vue-property-decorator';
     }
   }
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
   created() {
     this.$store.commit('fetchTags');
@@ -34,13 +36,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit('update:value', this.selectedTags);
-  }
-  create() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {
-      return window.alert('标签名不能为空');
-    }
-    this.$store.commit('createTag', name);
   }
 }
 </script>
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -16,18 +16,19 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 @Component({
-  components: {Button}
-})
-export default class Tag extends Vue {
-  tags = [];
-  // tags = store.tagList;
-  createTag() {
-    const name = window.prompt('请输入标签名');
-    if (name) {
-      // TODO
-      // store.createTag(name);
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
     }
+  }
+})
+export default class Tag extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit('fetchTags');
   }
 }
 </script>
