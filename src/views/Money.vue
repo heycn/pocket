f11bea13a65efca7da73c94af9cb015bc493f320
heycn <x1chennan@qq.com>
2021-12-15 19:18:14 +0800

    update

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,28 +16,31 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import oldStore from '@/store/index2.ts';
 
 @Component({
   components: {NumberPad, Types, FormItem, Tags},
   computed: {
     recordList() {
-      return this.$store.state.recordList;
+      return this.$store.state.count;
     }
   }
 })
 export default class Money extends Vue {
+  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [],
     notes: '',
     type: '-',
     amount: 0
   };
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
 
   saveRecord() {
-    this.$store.commit('createRecord', this.record);
+    oldStore.createRecord(this.record);
   }
 }
 </script>
