96691717be9b174dbcd5627f3f9ecfb89a41f857
heycn <x1chennan@qq.com>
2021-12-16 20:45:50 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
+    <Types :dataSource="recordTypeList" :value.sync="record.type" />
     <div class="notes">
       <FormItem fieldName="备注" placeholder="输入备注..." @update:value="onUpdateNotes" />
     </div>
@@ -10,45 +10,46 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import FormItem from '@/components/Money/FormItem.vue';
-import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
-@Component({
-  components: {NumberPad, Types, FormItem, Tags}
-})
-export default class Money extends Vue {
-  get recordList() {
-    return this.$store.state.recordList;
+  import Vue from 'vue';
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
+  import Tags from '@/components/Money/Tags.vue';
+  import {Component} from 'vue-property-decorator';
+  import recordTypeList from '@/constants/recordTypeList';
+  @Component({
+    components: {NumberPad, Types, FormItem, Tags}
+  })
+  export default class Money extends Vue {
+    get recordList() {
+      return this.$store.state.recordList;
+    }
+    recordTypeList = recordTypeList;
+
+    record: RecordItem = {
+      tags: [],
+      notes: '',
+      type: '-',
+      amount: 0
+    };
+    created() {
+      this.$store.commit('fetchRecords');
+    }
+    onUpdateNotes(value: string) {
+      this.record.notes = value;
+    }
+    saveRecord() {
+      this.$store.commit('createRecord', this.record);
+    }
   }
-  // TODO
-  // eslint-disable-next-line no-undef
-  record: RecordItem = {
-    tags: [],
-    notes: '',
-    type: '-',
-    amount: 0
-  };
-  created() {
-    this.$store.commit('fetchRecords');
-  }
-  onUpdateNotes(value: string) {
-    this.record.notes = value;
-  }
-  saveRecord() {
-    this.$store.commit('createRecord', this.record);
-  }
-}
 </script>
 
 <style lang="scss">
-.layout-content {
-  display: flex;
-  flex-direction: column-reverse;
-}
-.notes {
-  background: #f6f6f6;
-}
+  .layout-content {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+  .notes {
+    background: #f6f6f6;
+  }
 </style>
